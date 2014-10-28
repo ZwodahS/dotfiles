117c27596cd8ce405dae64913fb509b1529b35ad
Eric Ng <ericnjf@gmail.com>
2014-10-28 13:43:37 +0800

    update encrypt/decrypt script

--- bin/decrypt.sh
+++ bin/decrypt.sh
@@ -1,8 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ "$1" = "" ]
-then
-    echo "Enter file to decrypt"
-else
-    openssl des3 -salt -d -in "$1"
+if [ "$1" == '' ]; then
+    echo 'decrypt <file_in> <algorithm[des3|aes] $others'
+    exit 1
 fi
+
+FILE=$1
+ALGO=aes
+if [ "$2" == 'aes' -o "$2" == 'des' ]; then
+    ALGO=$2
+fi
+shift
+shift
+if [ "$ALGO" == 'aes' ]; then
+    ALGO='aes-256-cbc'
+elif [ "$ALGO" == 'des' ]; then
+    ALGO='des3'
+fi
+openssl $ALGO -a -d -salt -in $FILE $*
--- bin/encrypt.sh
+++ bin/encrypt.sh
@@ -1,8 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ "$1" = "" ]
-then
-    echo "Enter file to encrypt";
-else
-    openssl des3 -salt -out "$1".x -in "$1"
+if [ "$1" == '' ]; then
+    echo 'encrypt <file_in> <algorithm[des3|aes] $others'
+    exit 1
 fi
+
+FILE=$1
+ALGO=aes
+if [ "$2" == 'aes' -o "$2" == 'des' ]; then
+    ALGO=$2
+fi
+shift
+shift
+if [ "$ALGO" == 'aes' ]; then
+    ALGO='aes-256-cbc'
+elif [ "$ALGO" == 'des' ]; then
+    ALGO='des3'
+fi
+openssl $ALGO -a -salt -in $FILE -out ${FILE}.x $*
