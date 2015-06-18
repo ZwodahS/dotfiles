8dda25fa846d3eec5df7f11918bf3999fb13bf5e
Eric Ng <ericnjf@gmail.com>
2015-06-18 18:58:59 +0000

    Update tmux to show the commit hash for vimfile and dotfiles in tmux status

--- tmux/common.sh
+++ tmux/common.sh
@@ -10,3 +10,4 @@ function _tmux_title_formatted()
 {
     echo -n "#[fg=colour0,bg=colour120][#S]#[bg=default,fg=default]"
 }
+
--- tmux/status_right.sh
+++ tmux/status_right.sh
@@ -2,4 +2,12 @@
 FDR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$FDR/common.sh"
 
-echo -n "$(_format_time) "
+pushd ~/.vim > /dev/null
+vim_commit=$(git rev-parse HEAD | cut -c1-10 | tr -d '\n')
+popd > /dev/null
+
+pushd ~/.dotfiles > /dev/null
+dotfile_commit=$(git rev-parse HEAD | cut -c1-10 | tr -d '\n')
+popd > /dev/null
+
+echo -n "[d:#[fg=colour11]$dotfile_commit#[fg=default]] [v:#[fg=colour11]$vim_commit#[fg=default]] $(_format_time) "
