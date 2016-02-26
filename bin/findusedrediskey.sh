1c85ab0db44c3cb2187822fb3ed8ce1a2d4e0e31
Eric Ng <ericnjf@gmail.com>
2016-02-26 09:13:56 +0000

    add script to find used redis key

---
+++ bin/findusedrediskey.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" -o -z "$2" ]; then
+    echo "$(basename $0) <host> <port>"
+    exit 1
+fi
+
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15;
+do
+    STRING=$(redis-cli -h $1 -p $2 -n $i keys "*" | head -n 1)
+    if [ "$STRING" == "" ]; then
+        echo $i "not used"
+    else
+        echo $i "in use"
+    fi
+done
