18e2fc680e2d34a43e77d470f1adc90a9de78c9b
Eric Ng <ericnjf@gmail.com>
2017-04-19 04:43:47 +0000

    update find used redis keys script

--- bin/findusedrediskey.sh
+++ bin/findusedrediskey.sh
@@ -7,8 +7,8 @@ fi
 
 for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15;
 do
-    STRING=$(redis-cli -h $1 -p $2 -n $i keys "*" | head -n 1)
-    if [ "$STRING" == "" ]; then
+    STRING=$(redis-cli -h $1 -p $2 -n $i DBSIZE | cut -d ' ' -f 2)
+    if [ "$STRING" == "0" ]; then
         echo $i "not used"
     else
         echo $i "in use"
