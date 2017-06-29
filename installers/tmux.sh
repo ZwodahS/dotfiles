d8acbb2a4f68e988060b6900c7627c825e83055a
Eric Ng <ericnjf@gmail.com>
2017-06-29 14:41:15 +0800

    update tmux installer

--- installers/tmux.sh
+++ installers/tmux.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
-sudo add-apt-repository -y ppa:pi-rho/dev
-sudo apt-get update
-sudo apt-get install -y tmux=1.9a-1~ppa1~t
+SOURCE=$1
+
+if [ "$SOURCE" == "github" ]; then
+    git clone git@github.com:tmux/tmux --depth=1
+    pushd tmux
+    sudo apt-get install -y libevent-dev libncurses5-dev
+    ./configure
+    make
+    popd
+else
+    sudo add-apt-repository -y ppa:pi-rho/dev
+    sudo apt-get update
+    sudo apt-get install -y tmux=1.9a-1~ppa1~t
+fi
