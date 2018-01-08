3c1641e687517ece8bebb7328c3bdcdf6e4285a3
Eric Ng <ericnjf@gmail.com>
2018-01-08 21:53:43 +0800

    update tmux installer

--- installers/tmux.sh
+++ installers/tmux.sh
@@ -2,7 +2,11 @@
 SOURCE=$1
 
 if [ "$SOURCE" == "wget" ]; then
-    wget https://github.com/tmux/tmux/releases/download/2.5/tmux-2.5.tar.gz
+    if $(which wget); then
+        wget https://github.com/tmux/tmux/releases/download/2.5/tmux-2.5.tar.gz
+    else
+        curl -L -O https://github.com/tmux/tmux/releases/download/2.5/tmux-2.5.tar.gz
+    fi
     tar -xvf tmux-2.5.tar.gz
     pushd tmux-2.5
     sudo apt-get install -y libevent-dev libncurses5-dev
@@ -10,6 +14,8 @@ if [ "$SOURCE" == "wget" ]; then
     make
     sudo make install
     popd
+    rm tmux-2.5.tar.gz
+    rm -rf tmux-2.5
 else
     sudo add-apt-repository -y ppa:pi-rho/dev
     sudo apt-get update
