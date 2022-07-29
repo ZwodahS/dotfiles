def9c42f29f32b5b3196a0ba692386626b3bdd95
Eric Ng <ericnjf@gmail.com>
2022-07-29 12:25:55 +0800

    update

--- tmux/common.sh
+++ tmux/common.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 function _format_time()
 {
-    echo -n "#[fg=colour0,bg=colour120] $(TZ=Singapore date '+%H:%M') #[bg=default,fg=default] "
-    echo -n "#[fg=colour0,bg=colour120] #[bg=default,fg=default] "
-    echo -n "#[fg=colour0,bg=colour120] $(TZ=Singapore date '+%d %b %Y') #[bg=default,fg=default] "
+    echo -n "#[fg=#7aa2f7]"
+    echo -n "#[fg=#333333,bg=#7aa2f7] $(TZ=Singapore date '+%H:%M') "
+    echo -n "#[fg=#333333,bg=#7aa2f7] $(TZ=Singapore date '+%d %b %Y') #[bg=default,fg=default]"
 }
 
 function _tmux_title_formatted()
 {
-    echo -n "#[fg=colour0,bg=colour120][#S]#[bg=default,fg=default]"
+    echo -n "#[fg=#333333,bg=#7aa2f7][#S]#[bg=default,fg=default]"
+    echo -n "#[fg=#7aa2f7]"
 }
 
--- tmux/status_right.sh
+++ tmux/status_right.sh
@@ -15,4 +15,6 @@ if [ -n "$current_task" ]; then
     current_task = "Current Task: $current_task";
 fi
 
-echo -n "$current_task [d:#[fg=colour11]$dotfile_commit#[fg=default]] [v:#[fg=colour11]$vim_commit#[fg=default]] $(_format_time) "
+# [d:#[fg=colour11]$dotfile_commit#[fg=default]] [v:#[fg=colour11]$vim_commit#[fg=default]]
+
+echo -n "$current_task $(_format_time) "
