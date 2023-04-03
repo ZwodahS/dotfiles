29138549a1c05619585b3590cf5a11facd91b99c
Eric Ng <ericnjf@gmail.com>
2023-04-03 12:37:01 +0800

    add melange colorscheme

--- tmux/common.sh
+++ tmux/common.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+# Tokyonight theme
+# THEME_BG="7aa2f7"
+# THEME_FG="333333"
+# Melange
+THEME_FG="ECE1d7"
+THEME_BG="9a6062"
 function _format_time()
 {
-    echo -n "#[fg=#7aa2f7]"
-    echo -n "#[fg=#333333,bg=#7aa2f7] $(TZ=Singapore date '+%H:%M') "
-    echo -n "#[fg=#333333,bg=#7aa2f7] $(TZ=Singapore date '+%d %b %Y') #[bg=default,fg=default]"
+    echo -n "#[fg=#${THEME_BG}]"
+    echo -n "#[fg=#${THEME_FG},bg=#${THEME_BG}] $(TZ=Singapore date '+%H:%M') "
+    echo -n "#[fg=#${THEME_FG},bg=#${THEME_BG}] $(TZ=Singapore date '+%d %b %Y') #[bg=default,fg=default]"
 }
 
 function _tmux_title_formatted()
 {
-    echo -n "#[fg=#333333,bg=#7aa2f7][#S]#[bg=default,fg=default]"
-    echo -n "#[fg=#7aa2f7]"
+    echo -n "#[fg=#${THEME_FG},bg=#${THEME_BG}][#S]#[bg=default,fg=default]"
+    echo -n "#[fg=#${THEME_BG}]"
 }
-
